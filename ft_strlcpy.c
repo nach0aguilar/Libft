4d77ef14c1e11d5d1f7b30d77abf5f458d85a4ce
nach0aguilar <nachoaguilarfdz@gmail.com>
2023-09-17 17:11:26 +0200

    correccion strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,13 +6,38 @@
 /*   By: igaguila <igaguila@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 14:13:32 by igaguila          #+#    #+#             */
-/*   Updated: 2023/09/16 14:18:57 by igaguila         ###   ########.fr       */
+/*   Updated: 2023/09/17 16:19:24 by igaguila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include <stdio.h>
 
 size_t  ft_strlcpy(char *dst, const char *src, size_t size)
 {
+    size_t  i;
+    size_t  j;
+
+    j = 0;
+    while
     
+    i = 0;
+    while (i < size)
+    {
+        dst[i] = src[i];
+        i++;
+    }
+    dst[i] = '\0';
+    return (sizeof(src));
 }
+
+int main()
+{
+    char source[] = "Hola que tal";
+    char dest[15];
+
+    strlcpy(dest, source, sizeof(dest));
+
+    printf("%s\n", dest);
+}
+
