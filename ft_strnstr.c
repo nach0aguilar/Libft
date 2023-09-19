653ad3ccc06b7711a6d36a7399407529256815e3
nach0aguilar <nachoaguilarfdz@gmail.com>
2023-09-19 11:39:57 +0200

    pruebas strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: igaguila <igaguila@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 20:10:09 by igaguila          #+#    #+#             */
-/*   Updated: 2023/09/18 21:12:41 by igaguila         ###   ########.fr       */
+/*   Updated: 2023/09/19 10:33:14 by igaguila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,10 @@
 
 int main()
 {
-    char ciudades[] = "madrid malaga sevilla aragon cadiz";
-    char ciudad [] = "malaga";
-    char *resultado = (char *)strnstr(ciudades, ciudad, 2);
+    char *ciudades = "madrid malaga sevilla aragon cadiz";
+    char *ciudad = "malaga";
+    
+    char *resultado = strnstr(ciudades, ciudad, 23);
 
     if(resultado != NULL)
 	    printf("%s\n", resultado);
