ff7d82c0fa9d7b5b93a948ec8f67734694657fc4
nach0aguilar <nachoaguilarfdz@gmail.com>
2023-09-26 12:28:42 +0200

    continuación de split

--- ft_split.c
+++ ft_split.c
@@ -6,31 +6,67 @@
 /*   By: igaguila <igaguila@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 12:58:52 by igaguila          #+#    #+#             */
-/*   Updated: 2023/09/25 22:49:44 by igaguila         ###   ########.fr       */
+/*   Updated: 2023/09/26 12:18:57 by igaguila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "libft.h"
 
-char *make_word(char const *s, char c, int pos)
+static int	words(char const *s, char c)
 {
+	int	words;
+	int inword;
+	int i;
+	
+	words = 0;
+	inword = 0;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] != c && inword == 0)
+		{
+			inword = 1;
+			words++;
+		}
+		else if (s[i] == c)
+			inword = 0;
+		i++;
+	}
+	return (words);
+}
+
+static char *make_word(char const *s, char c, int pos)
+{
+	int i;
+	int j;
 	char *str;
 	
+	j = 0;
+	i = pos;
+	while (s[pos] != c)
+		pos++;
 	
+	str = malloc(sizeof(char) * (pos - i + 1));
+	if(!str)
+		return (0);
+	while (s[i] != c)
+		str[j++] = s[i++];
+	return (str);	
 }
 
 char	**ft_split(char const *s, char c)
 {
-	int		i;
+	size_t	i;
 	int		j;
 	char	**array;
 
 	i = 0;
 	j = 0;
-	array = (char **)malloc(50 * sizeof(char *));
-	if (!array)
-		return (NULL);
+	array = (char **)malloc(sizeof(char *) * (words(s, c) + 1));
+	if (!array || !s)
+		return (0);
 	while (s[i])
 	{
 		if (s[i] == c)
@@ -42,7 +78,6 @@ char	**ft_split(char const *s, char c)
 		}
 	}
 	array[j] = 0;
-	//array[x] = 0;
 	return (array);
 }
 
